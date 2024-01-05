50c9839cb4b0fe762c204f2ff1b79e9535a9cabe
ashsajal1 <ashsajal1@gmail.com>
2024-01-05 14:16:10 +0800

    Add navabar component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,7 +9,6 @@ interface InputProps {
 export default function Input({inputs, loading, handleInputChange }: InputProps) {
     return (
         <textarea value={inputs} disabled={loading} onChange={handleInputChange} className="w-full h-[200px] border p-2 rounded outline-none text-sm focus:ring-1 focus:ring-violet-600 transition-all duration-300 text-violet-600" placeholder="Enter text">
-            <span>This is ai generated text</span>
         </textarea>
     )
 }
