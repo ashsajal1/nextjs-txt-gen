b1dc4ff31ede50fd587074587b9d6f00c2adabdf
ashsajal1 <ashsajal1@gmail.com>
2024-01-05 12:15:52 +0800

    Add hanldeSubmit function on button prosp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,22 @@ import { ChangeEvent, useState } from "react"
 
 export default function Home() {
   const [inputs, setInputs] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('ffdas');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
   const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setInputs(e.target.value)
-    console.log(e.target.value)
+    // console.log(e.target.value)
+  };
+
+  const handleSubmit = async () => {
+    console.log("first")
   }
+
   return (
     <main className="flex flex-col items-center justify-between p-24 gap-4">
-     <Input loading={loading} handleInputChange={handleInputChange} />
-      <Button loading={loading} />
-
-      {/* {loading && (
-        <div className="animate-pulse p-2 bg-violet-300">Loading ...</div>
-      )} */}
+      <Input loading={loading} handleInputChange={handleInputChange} />
+      <Button handleSubmit={handleSubmit} loading={loading} />
 
       {error && (
         <Error error={error} />
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import GenerateIcon from './GenerateIcon'
 
-export default function Button({loading} : {loading: boolean}) {
+export default function Button({loading, handleSubmit} : {loading: boolean, handleSubmit : () => void}) {
     return (
-        <button disabled={loading} className={`bg-violet-600 p-2 rounded w-full text-slate-50 flex items-center justify-center gap-2 hover:bg-violet-700 transition-all duration-300 cursor-pointer text-sm disabled:bg-violet-400 ${loading ? "animate-pulse" : ""}`}>
+        <button onClick={handleSubmit} disabled={loading} className={`bg-violet-600 p-2 rounded w-full text-slate-50 flex items-center justify-center gap-2 hover:bg-violet-700 transition-all duration-300 cursor-pointer text-sm disabled:bg-violet-400 ${loading ? "animate-pulse" : ""}`}>
             <GenerateIcon />
             <span>{loading ? "Generating" : "Generate"}</span>
         </button>
