263f98eee7930cb3207f3d5f9747076582bcc926
ashsajal1 <ashsajal1@gmail.com>
2024-01-05 14:39:05 +0800

    Add env variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    HUGGING_FACE_KEY: "Bearer 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",
+    HUGGING_FACE_MODEL: "https://api-inference.huggingface.co/models/gpt2",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
