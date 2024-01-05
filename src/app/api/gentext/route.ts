5d61e93c97f134f7026a9faf470e0e76bee2f413
ashsajal1 <ashsajal1@gmail.com>
2024-01-05 11:18:30 +0800

    Added api route

---
+++ src/app/api/gentext/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  const { inputs } = await req.json();
+
+  try {
+    const response = await fetch(process.env.HUGGING_FACE_MODEL as string, {
+      headers: { Authorization: process.env.HUGGING_FACE_KEY as string },
+      method: "POST",
+      body: JSON.stringify({
+        inputs,
+      }),
+    });
+    const result = await response.json();
+
+    return NextResponse.json({
+      ok: true,
+      result,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      ok: false,
+      error,
+    });
+  }
+};
