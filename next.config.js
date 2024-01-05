050da19fe9e71873b86a42fd8083bd5730e064e2
ashsajal1 <ashsajal1@gmail.com>
2024-01-05 14:41:25 +0800

    Make env variable private

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    HUGGING_FACE_KEY: "Bearer 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",
-    HUGGING_FACE_MODEL: "https://api-inference.huggingface.co/models/gpt2",
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
